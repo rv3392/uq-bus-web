0aa3fbba117b2c4b163ac1295279c23ed7ba2195
rv3392 <richalverma00@gmail.com>
2020-03-15 14:51:35 +1000

    Added github-pages deploy info

--- src/index.js
+++ src/index.js
@@ -2,66 +2,59 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
+//Gets bus details from 'trips' table using a trip_id
+//trip_id is one of the next 'x' busses departing from uq (stop_times)
+//Gets further bus details from 'routes' table using a route_id found in
+//'trips'
+
+const uqStops = ["1853", "1878", "1880", "1882", "1883"];
+
+class App extends React.Component {
+
+  state = {
+    buses: []
+  }
+
+  //, "1877", "1878", "1880", "1882", "1883"];
+
+  //var today = Date();
+  //var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
+  async getBusesForStop(stopId) {
+    const tripIdResponse =
+        await fetch("http://uqbus.richal.tech/stop_time/stop_id/" + stopId + "/");
+
+    const trips = await tripIdResponse.json()
+    var tripsArray = []
+
+    for (var i = 0; i < 5; i++) {
+      tripsArray.push(trips[i].trip_id);
+    }
+
+    this.setState({
+      buses: tripsArray
+    });
+  }
+
+  getBuses() {
+    uqStops.forEach(stopId => this.getBusesForStop(stopId))
+  }
+
+  getBusListElement() {
+    this.getBuses();
+
     return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
+      this.state.buses.map(bus => 
+        <li key = {bus}>{bus}</li>
+      ));
+  }
+
+  render() {
+    return this.getBusListElement();
   }
 }
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
-
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-// ========================================
 
 ReactDOM.render(
-  <Game />,
+  <App></App>,
   document.getElementById('root')
 );
