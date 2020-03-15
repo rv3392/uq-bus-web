b3daee00d5c720461b38eccc7e6514fe28fa8ef2
rv3392 <richalverma00@gmail.com>
2020-03-15 16:26:30 +1000

    Updates

---
+++ precache-manifest.e5499888534b88b047265ea9ac7910b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe29fcd6c5db8eb0896e6ffe05fd1a6",
+    "url": "/uq-bus-web/index.html"
+  },
+  {
+    "revision": "8630922320f8163e94f0",
+    "url": "/uq-bus-web/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "826281ce259baa0a70c6",
+    "url": "/uq-bus-web/static/js/2.d1a19dde.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/uq-bus-web/static/js/2.d1a19dde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8630922320f8163e94f0",
+    "url": "/uq-bus-web/static/js/main.5cc53d9c.chunk.js"
+  },
+  {
+    "revision": "9b584e9d3c323e7a958f",
+    "url": "/uq-bus-web/static/js/runtime-main.64a13ae0.js"
+  }
+]);
